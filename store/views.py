34a046633ca0ba1d6366521e809b36a0e6e16a64
Venish P <sec19ec070@sairamtap.edu.in>
2021-12-05 01:33:55 +0530

    fifth commit

--- store/views.py
+++ store/views.py
@@ -1,14 +1,34 @@
 from django.shortcuts import render
 
+from .models import *
 
 def store(request):
-    context = {}
+    products = Product.objects.all()
+    context = {'products': products}
     return render(request, 'store/store.html', context)
 
 def cart(request):
-    context = {}
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all() # returns orderitem for this order
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+    context = {'items': items, 'order': order}
     return render(request, 'store/cart.html', context)
 
 def checkout(request):
-    context = {}
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        # Create Empty cart for now for none-logged in users
+        order = { 'get_cart_items':0, 'get_cart_total':0 }
+        items = []
+    
+    context = {'items': items, 'order': order}
     return render(request, 'store/checkout.html', context)
