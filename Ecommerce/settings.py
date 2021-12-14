3daa3dd7d0dd73b5d007c5dd0c73d511638891cd
Venish P <sec19ec070@sairamtap.edu.in>
2021-12-15 00:42:12 +0530

    connected to mysql

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'Ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ecomdatabase',
+        'USER': 'dbadmin',
+        'PASSWORD': 'admin@1-100',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
